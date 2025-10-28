a6a054996f4396f1925d0c65d5e831244e431b58
zafkiel45 <zafkielc@gmail.com>
2025-10-28 16:19:31 -0300

    hashMap of fruits exercise added

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,13 @@ fn main() {
     voted(&mut vote, String::from("Murilo"));
     voted(&mut vote, String::from("Emilly"));
     voted(&mut vote, String::from("Murilo"));
+
+    let mut fruits: HashMap<String, f64> = HashMap::new();
+
+    insert_or_incress_fruit_value("banana".to_string(), &mut fruits);
+    insert_or_incress_fruit_value("apple".to_string(), &mut fruits);
+    insert_or_incress_fruit_value("barry".to_string(), &mut fruits);
+    insert_or_incress_fruit_value("apple".to_string(), &mut fruits);
 }
 
 fn voted(list: &mut HashMap<String, bool>, name: String) -> bool {
@@ -19,4 +26,18 @@ fn voted(list: &mut HashMap<String, bool>, name: String) -> bool {
     }
 
     false 
+}
+
+fn insert_or_incress_fruit_value(fruit: String, list: &mut HashMap<String, f64>) {
+    if list.contains_key(&fruit) {
+        list.entry(fruit.clone()).and_modify(|p| *p += 1.0);
+
+        println!("Price of {} was incresed in: {}", fruit, 1.0);
+    } else {
+        let price: f64 = 3.0;
+
+        list.insert(fruit.clone(), price);
+
+        println!("Fruit: {} added with price: {}", fruit, price);
+    };
 }
